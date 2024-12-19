d2e5f0924c2340afaa7e99f00789a251ff0a4733
Ian Jiang <ian.jiang@domain.com.au>
2024-12-20 10:12:59 +1100

    Initial commit

---
+++ CatFact/CatFact/CatFactApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatFactApp.swift
+//  CatFact
+//
+//  Created by Ian Jiang on 20/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CatFactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
